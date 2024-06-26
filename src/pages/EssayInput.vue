d47ae4c468fcd9a6ea3ab5f6db7b75a3a6241926
SooHwanC <frcp9408@gmail.com>
2024-06-26 15:26:45 +0900

    0626_essayinput

---
+++ src/pages/EssayInput.vue
@@ -0,0 +1,73 @@
+<template>
+<div class="Main">
+    <left-bar />
+    <div class="right_wrapper">
+        <div class="ex_content_wrapper">
+            <div class="ex_content_box">
+                <div class="job_input_wrapper">
+                    <div class="job_input_box">
+                        <h2>1. 성장과정</h2>
+                        <div class="text_wrapper">
+                            <textarea placeholder="성장과정을 입력해주세요."></textarea>
+                        </div>
+                        <div class="charCount">
+                            0/1000
+                        </div>
+                    </div>
+                    <div class="job_input_box">
+                        <h2>2. 성격의 장단점</h2>
+                        <div class="text_wrapper">
+                            <textarea placeholder="성격의 장단점을 입력해주세요."></textarea>
+                        </div>
+                        <div class="charCount">
+                            0/1000
+                        </div>
+                    </div>
+                    <div class="job_input_box">
+                        <h2>3. 지원동기</h2>
+                        <div class="text_wrapper">
+                            <textarea placeholder="지원동기를 입력해주세요."></textarea>
+                        </div>
+                        <div class="charCount">
+                            0/1000
+                        </div>
+                    </div>
+                    <div class="job_input_box">
+                        <h2>4. 입사 후 포부</h2>
+                        <div class="text_wrapper">
+                            <textarea placeholder="입사 후 포부를 입력해주세요."></textarea>
+                        </div>
+                        <div class="charCount">
+                            0/1000
+                        </div>
+                    </div>
+                </div>
+                <div class="submit_btn_wrapper">
+                    <div class="submit_btn_box">
+                        제출하기
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+</template>
+
+<script>
+import LeftBar from '../components/LeftBar.vue'
+import "@/SCSS/pages/EssayInput.scss"
+
+export default {
+    components: {
+        LeftBar
+    },
+
+}
+</script>
+
+<style>
+
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Main from '../pages/Main.vue';
 import Join from '../pages/Join.vue';
 import Login from '../pages/Login.vue';
+import EssayInput from '../pages/EssayInput.vue'
 
 const routes = [
     { path: '/', name: 'Main', component: Main },
     { path: '/join', name: 'Join', component: Join },
-    { path: '/login', name: 'Login', component: Login }
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/essayinput', name: 'EssayInput', component: EssayInput }
 ]
 
 const router = createRouter({
