aa908300d62fa6524adb8525fa18fb126d09870d
SooHwanC <frcp9408@gmail.com>
2024-07-02 16:48:28 +0900

    회원가입

--- src/pages/Join.vue
+++ src/pages/Join.vue
@@ -12,28 +12,73 @@
       <div class="input_wrapper">
         <div class="input_box">
           <h3>아이디</h3>
-          <input type="text" placeholder="아이디를 입력해주세요." />
+          <input type="text" v-model="member_id" placeholder="아이디를 입력해주세요." />
         </div>
         <div class="input_box">
           <h3>비밀번호</h3>
-          <input type="password" placeholder="비밀번호를 입력해주세요." />
+          <input type="password" v-model="password" placeholder="비밀번호를 입력해주세요." />
         </div>
         <div class="input_box">
           <h3>비밀번호 확인</h3>
           <input type="password" placeholder="비밀번호를 한번 더 입력해주세요." />
         </div>
       </div>
+
       <div class="submit_btn_box">
-        <div class="submit_btx">회원가입</div>
+        <div class="submit_btx" @click="handleJoin">회원가입</div>
       </div>
+
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 import "../SCSS/pages/Join.scss";
+import { useRouter } from "vue-router";
+import axios from "axios";
 
-export default {};
+export default {
+  setup() {
+    const member_id = ref('');
+    const password = ref('');
+    const router = useRouter();
+
+    const handleJoin = async () => {
+
+      try {
+        const response = await axios.post(`${import.meta.env.VITE_API_BACK_URL}/member/join`, {
+          member_id: member_id.value,
+          password: password.value
+        })
+
+        if (response.status === 200) {
+          alert(response.data)
+          router.push('/login')
+        }
+
+      } catch (error) {
+        if (error.response.status === 409) {
+          alert(error.response.data)
+        }
+
+      }
+    }
+
+    return {
+      member_id,
+      password,
+      handleJoin
+
+    }
+
+
+
+
+  }
+
+
+};
 </script>
 
 <style></style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,82 +1,118 @@
 <template>
-<div class="login_main_wrapper">
-    <div class="login_main">
-        <div class="logo_wrapper">
-            <img src="../assets/images/CI_심볼마크.png" alt="logo" />
-        </div>
+    <div class="login_main_wrapper">
+        <div class="login_main">
+            <div class="logo_wrapper">
+                <img src="../assets/images/CI_심볼마크.png" alt="logo" />
+            </div>
 
-        <div class="title_wrapper">
-            <h1>로그인</h1>
-        </div>
+            <div class="title_wrapper">
+                <h1>로그인</h1>
+            </div>
 
-        <form @submit.prevent="handleLogin" class="login_form">
-            <div class="input_wrapper">
-                <div class="input_box">
-                    <h3>아이디</h3>
-                    <input type="text" v-model="username" placeholder="아이디를 입력해주세요." />
+            <form @submit.prevent="handleLogin" class="login_form">
+                <div class="input_wrapper">
+                    <div class="input_box">
+                        <h3>아이디</h3>
+                        <input type="text" v-model="username" placeholder="아이디를 입력해주세요." />
+                    </div>
+                    <div class="input_box">
+                        <h3>비밀번호</h3>
+                        <input type="password" v-model="password" placeholder="비밀번호를 입력해주세요."
+                            @keyup.enter="handleLogin" />
+                    </div>
                 </div>
-                <div class="input_box">
-                    <h3>비밀번호</h3>
-                    <input type="password" v-model="password" placeholder="비밀번호를 입력해주세요." @keyup.enter="handleLogin" />
+                <div class="submit_btn_box">
+                    <div class="submit_btx" @click="handleLogin">로그인</div>
                 </div>
-            </div>
-            <div class="submit_btn_box">
-                <div class="submit_btx" @click="handleLogin">로그인</div>
-            </div>
-        </form>
-        <div class="social_login_wrapper">
-            <div class="kakao_login_box">
-                <img src="../assets/images/kakao_loginsvg.svg" alt="kakao_login" />
-            </div>
-            <!-- <h2>or</h2> -->
-            <div class="google_login_box">
-                <img src="../assets/images/web_light_sq_SI.svg" alt="google_login" />
+            </form>
+            <div class="social_login_wrapper">
+                <div class="kakao_login_box">
+                    <img src="../assets/images/kakao_loginsvg.svg" alt="kakao_login" />
+                </div>
+                <!-- <h2>or</h2> -->
+                <div class="google_login_box">
+                    <img src="../assets/images/web_light_sq_SI.svg" alt="google_login" />
+                </div>
             </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
+import { ref } from "vue";
 import "../SCSS/pages/Login.scss";
 import axios from 'axios';
+import { useRouter } from "vue-router";
 
 export default {
-    data() {
-        return {
-            username: "",
-            password: "",
-        };
-    },
+    setup() {
+        const username = ref('');
+        const password = ref('');
+        const router = useRouter();
 
-    methods: {
-        async handleLogin() {
+        const handleLogin = async () => {
             try {
                 const response = await axios.post(`${import.meta.env.VITE_API_BACK_URL}/member/login`, {
-                    member_id: this.username,
-                    password: this.password
+                    member_id: username.value,
+                    password: password.value
                 });
                 if (response.status === 200) {
-                    sessionStorage.setItem('member_id', this.username);
+                    sessionStorage.setItem('member_id', username.value);
                     alert('로그인 성공');
-                    this.$router.push('/');
-                    
+                    router.push('/');
                 }
-
             } catch (error) {
-                if(error.response.status === 404){
-                    alert(error.response.data)
-                } else if (error.response.status === 403){
+                if (error.response.status === 404) {
+                    alert(error.response.data);
+                } else if (error.response.status === 403) {
                     alert(error.response.data)
                 }
             }
-
         }
 
+        return {
+            username,
+            password,
+            handleLogin
+        }
     }
+
+
+
+    // data() {
+    //     return {
+    //         username: "",
+    //         password: "",
+    //     };
+    // },
+
+    // methods: {
+    //     async handleLogin() {
+    //         try {
+    //             const response = await axios.post(`${import.meta.env.VITE_API_BACK_URL}/member/login`, {
+    //                 member_id: this.username,
+    //                 password: this.password
+    //             });
+    //             if (response.status === 200) {
+    //                 sessionStorage.setItem('member_id', this.username);
+    //                 alert('로그인 성공');
+    //                 this.$router.push('/');
+
+    //             }
+
+    //         } catch (error) {
+    //             if(error.response.status === 404){
+    //                 alert(error.response.data)
+    //             } else if (error.response.status === 403){
+    //                 alert(error.response.data)
+    //             }
+    //         }
+
+    //     }
+
+    // }
 
 };
 </script>
 
-<style>
-</style>
+<style></style>
